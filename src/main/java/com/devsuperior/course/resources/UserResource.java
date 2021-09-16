bcb7459842653784d9fa78f634662d3e74739237
Leandro Henrique <leandrohamorim@outlook.com>
2021-09-16 00:43:36 -0300

    User entity and resource

---
+++ src/main/java/com/devsuperior/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.devsuperior.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Leandro","leandro@email.com","81983124781","123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}

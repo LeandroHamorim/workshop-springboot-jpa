8b797078e9423a06395b0b7f32c243e85d3e0d95
Leandro Henrique <leandrohamorim@outlook.com>
2021-09-20 00:56:38 -0300

    Product

--- src/main/java/com/devsuperior/course/services/CategoryService.java
+++ src/main/java/com/devsuperior/course/services/CategoryService.java
@@ -1,24 +1,27 @@
 package com.devsuperior.course.services;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.course.entities.Category;
+import com.devsuperior.course.entities.Product;
 import com.devsuperior.course.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
+
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
